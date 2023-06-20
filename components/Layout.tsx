903603b57813531fb8b1a853535d9b8674a34a66
khkmgch <kohei.kamiguchi@gmail.com>
2023-06-20 23:49:48 +0900

    add: Layout

---
+++ components/Layout.tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head';
+import React, { FC, ReactNode } from 'react';
+import { Navbar } from './Navbar';
+import { Footer } from './Footer';
+import { NextSeo } from 'next-seo';
+
+type Props = {
+  children: ReactNode;
+};
+export const Layout: FC<Props> = ({ children }) => {
+  const title: string = "koh-secret-hideaway";
+  const description: string =
+    "This is Koh's personal website";
+  const url: string | undefined =
+    process.env.NEXT_PUBLIC_URL;
+  const twitter_name: string | undefined =
+    process.env.NEXT_PUBLIC_TWITTER_NAME;
+  return (
+    <>
+      <Head>{title}</Head>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={url}
+        openGraph={{
+          url: url,
+          title: title,
+          description: description,
+          images: [
+            {
+              url: '/vercel.svg',
+              width: 800,
+              height: 600,
+              alt: title,
+            },
+          ],
+          siteName: title,
+        }}
+        twitter={{
+          handle: twitter_name,
+          site: twitter_name,
+          cardType: 'summary_large_image',
+        }}
+      />
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
